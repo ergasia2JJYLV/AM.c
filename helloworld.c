83851c50274802b96fdbbb88c21a6b2a8e141200
Nikoklis <nikoklis2@hotmail.gr>
2015-12-17 14:17:00 +0200

    hellow

--- helloworld.c
+++ helloworld.c
@@ -2,12 +2,5 @@
 
 int main(){
   printf("helloworld\n");
-<<<<<<< HEAD
-  printf("HelloJJ\n" );
-  printf("Fuji\n");
-  return 0;
-
-=======
-  printf("What the fuck\n" );
->>>>>>> 97afe0aa56b08db6d15c2486f9319ff8b1d0ec9b
+  printf("asdsad\n" );
 }
