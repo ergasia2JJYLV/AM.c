6e7014b2bb8e0640291a472add431273090cbd42
flyingpeanut <mangateersd@hotmail.com>
2015-12-18 19:06:17 +0200

    initialization of root

--- Open_Index.c
+++ Open_Index.c
@@ -1,16 +1,17 @@
+#include <stdio.h>
 #include "AM.h"
 #include "defn.h"
 #include "BF.h"
-#include <stdio.h>
-
+#include "Btree.h"
 #define MAXOPENFILES 20
 
 struct FileInfo{
-  int i;
-  int keytype;
-  int datatype;
-  int datacapacity; // plh8os kleidiwn+data pou xwrane se ena block 
-  int keycapacity; // plh8os kleidiwn pou xwrane se ena block
+  int   i;
+  int   keytype;
+  int   datatype;
+  int   datacapacity; // plh8os kleidiwn+data pou xwrane se ena block 
+  int   keycapacity; // plh8os kleidiwn pou xwrane se ena block
+  node* root;         
 };
 
 typedef struct FileInfo FileInfo;
@@ -45,7 +46,7 @@ int AM_OpenIndex(char *fileName)
       return AM_errno=AM_ERCREATE;
   }
   
-  // type string =0  , int = 1, float = 2 
+  // type string 0> , int = -1, float = -2 
   //  string max size =255, int/float =4
 
   // key type-size
@@ -54,8 +55,76 @@ int AM_OpenIndex(char *fileName)
   // value type-size
   OpenFile[i].keycapacity=((int*)block)[2];
   OpenFile[i].datacapacity=((int*)block)[3];
+
+
+  int size;
+  if(OpenFile[i].keytype==-1){
+    size=sizeof(int);
+  }
+  else if(OpenFile[i].keytype==-2){
+    size=sizeof(float);
+  }
+  else {
+    size=OpenFile[i].keytype;
+  }
+
+  if(BF_ReadBlock(fileDesc,1, &block)<0){
+      fprintf(stderr,"can't read the file #%d \n",fileDesc);
+      return AM_errno=AM_ERCREATE;
+  }
+
+
+  OpenFile[i].root  =malloc(sizeof(node)); // malloc rizas
+  OpenFile[i].root->block=malloc(sizeof(int)*(OpenFile[i].keycapacity+1));  // malloc pinaka int pou periexe block #
+  OpenFile[i].root->key=malloc(size*(OpenFile[i].keycapacity));             // malloc pinaka me keys 
+  OpenFile[i].root->branch=malloc(sizeof(Node)*(OpenFile[i].keycapacity+1));// malloc pinaka me ta branches;
+  if(OpenFile[i].root==NULL){
+    printf("error with memory allocation\n");
+    return AM_errno=AM_ERCREATE;
+  }
+  if(OpenFile[i].root->block==NULL){
+    printf("error with memory allocation\n");
+    return AM_errno=AM_ERCREATE;
+  }
+  if( OpenFile[i].root->key==NULL){
+    printf("error with memory allocation\n");
+    return AM_errno=AM_ERCREATE;
+  }
+  if(OpenFile[i].root->branch==NULL){
+    printf("error with memory allocation\n");
+    return AM_errno=AM_ERCREATE;
+  }
+
+  OpenFile[i].root->nextBlock=NULL;                                         // H riza exei dn exei next
+  OpenFile[i].root->checking=0;                                             // check ==0
+
+  if(((int*)block)[0]==-1){   // root is empty
+    int i;
+    for(i=0;i<keycapacity;i++){   // MHDENISE TA PANTA
+
+        OpenFile[i].root->branch[i]=0;
+        OpenFile[i].root->block[i]=0;
+
+        if(size==sizeof(int)|| size==sizeof(float)){ 
+            OpenFile[i].root->key[i]==0;
+        }
+        else{
+            OpenFile[i].root->key[i]=="";
+        }
+    }
+    // keycapacity +1 periptwsh
+    OpenFile[i].root->branch[i]=0;
+    OpenFile[i].root->block[i]=0;
+
+  }
+  else{ // pare ta data apo to block tou root ... 
+
+
+  }
+
   printf("%d %d %d %d \n", OpenFile[i].keytype, OpenFile[i].datatype, OpenFile[i].keycapacity, OpenFile[i].datacapacity);
   printf("BFS is %d\n",bfs );
+
   return OpenFile[i].i;
 }
 
